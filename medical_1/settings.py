4314d90ec842517e5cb1d95de973f494b797ff93
Its_Devvrat <77926218+devvrat4522-kan@users.noreply.github.com>
2024-06-24 21:56:21 +0530

    Update settings.py

--- medical_1/settings.py
+++ medical_1/settings.py
@@ -33,6 +33,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
+
+
 
 # Application definition
 
