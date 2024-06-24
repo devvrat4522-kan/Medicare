f94a9a213a17459289ada43a3c502fe5efb5db48
Its_Devvrat <77926218+devvrat4522-kan@users.noreply.github.com>
2024-06-24 20:22:30 +0530

    Update settings.py

--- medical_1/settings.py
+++ medical_1/settings.py
@@ -16,6 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+PORT = os.environ.get('PORT', 8000)
+
+# Update ALLOWED_HOSTS
+ALLOWED_HOSTS = ['*']
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
